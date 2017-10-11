7d72a3878c5c50f8da181d3344d9179bae1ddad8
emilyyleung <emily_y_leung@outlook.com>
2017-10-11 15:08:54 +1100

    171011
    3:06PM
    
    Can now click on an object in the viewport, select it, and show it's user data properties in a table under "Data Table". Problem is that I cannot adjust the look to show the data in a transverse position as well as viewing data objects within data objects. It will only display [object object] in the cell.

--- 170919/src/App.js
+++ 170919/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+
 // import * as THREE from 'three';
 import * as helpers from './util/helpers.js';
 import FluxViewport from 'flux-viewport/dist/flux-viewport.common.js';
@@ -79,10 +80,56 @@ class App extends Component {
         var selectedGeometryObject = selectedGeometry[this.x];
       }
       if(selectedGeometryObject !== null && selectedGeometryObject !== undefined) {
-        console.log(selectedGeometryObject);
-        console.log(JSON.stringify(selectedGeometryObject.userData.data));
+        // console.log(selectedGeometryObject);
+        // console.log(JSON.stringify(selectedGeometryObject.userData.data));
+
+        // EXTRACT VALUE FOR HTML HEADER.
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        var myData = JSON.parse("[" + JSON.stringify(selectedGeometryObject.userData.data) + "]" );
+
+        var col = [];
+        for (var i = 0; i < myData.length; i++) {
+            for (var key in myData[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < myData.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = myData[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+
+
+        console.log(myData);
       } else {
         console.log("Nothing")
+        $( "#showData" ).empty();
       }
     });
 }
@@ -232,28 +279,10 @@ createClippingPlane(e, json) {
   e.preventDefault();
 
   if(this.project !== null && this.state.value !== null) {
-    if (!FluxViewport.isKnownGeom(json)) {
       console.log("Clipping Plane");
-    }
-
-    // var p = new THREE.Vector3(0, 5, 0);
-    // var n = new THREE.Vector3(0, 10, 0);
-    // var d = new THREE.Vector3(0, -5, 0);
-    // var o = new THREE.Vector3(0, 0, 0);
-    //
-    // var px = 10;
-    // var py = 10;
-    // var pz = 10;
-    // var nx = 10;
-    // var ny = 10;
-    // var nz = 10;
-    //
-    // this.vp.activateClipping(px, py, pz, nx, ny, nz);
-
   } else {
     alert("Please Select a project and key")
   }
-
 }
 
   render() {
@@ -274,6 +303,12 @@ createClippingPlane(e, json) {
             <hr/>
             <KeyList test="Hello World" keyList={this.state.keys} keyData={this.state.data} />
             <hr />
+
+            <h3>Data Table</h3>
+            <div id="showData" className="showDataTable" ></div>
+
+            <hr/>
+
             <div className="AddKey">
               <form onSubmit={this.handleSubmitKey.bind(this)} >
                 <h3>Add Key</h3>
@@ -312,6 +347,8 @@ createClippingPlane(e, json) {
             </form>
 
             <hr/>
+
+
             <div id="notifications">
               <h3>Update Log</h3>
               <textarea id="console" className="fields" type="text" onChange={(e)=>{this._handleDataChange(e)}} value={this.notificationHandler} ref={(area)=>{this._comment = area;}} name="Data" ></textarea>
